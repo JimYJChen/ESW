490c96de86e590106a81321480cdf8e81a8493b8
jimyjchen@aliyun.com <chenyingjie123>
2018-12-13 14:54:37 +0800

    Init commit

---
+++ src/main/java/com/demo/springesw/SpringeswApplication.java
@@ -0,0 +1,14 @@
+package com.demo.springesw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringeswApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringeswApplication.class, args);
+    }
+
+}
+
